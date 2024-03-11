0cfff1546c0a92dfc0e063784b5b7084a194061f
ahmadlatheef <ahmad.lathf@gmail.com>
2024-03-11 14:44:41 +0530

    creating relationships

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
