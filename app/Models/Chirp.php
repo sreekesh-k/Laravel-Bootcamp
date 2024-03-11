4a6016081708899b1d846592a1a1a18478ae5a6b
ahmadlatheef <ahmad.lathf@gmail.com>
2024-03-11 14:51:05 +0530

    relation fix

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,17 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Relations\HasMany;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
-    public function chirps(): HasMany
-    {
-        return $this->hasMany(Chirp::class);
-    }
     protected $fillable = [
         'message',
     ];
